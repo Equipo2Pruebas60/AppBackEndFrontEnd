a9cd5cc41388263d04595ac3909b35727fb0a8d2
GlendaPimienta31 <91032231+GlendaPimienta31@users.noreply.github.com>
2021-10-19 06:20:53 -0500

    Reportes.java

---
+++ tiendavirtual/src/principal/Java/com/ubosque/DTO/Reportes.java
@@ -0,0 +1,30 @@
+package com.ubosque.DTO;
+
+public class Reportes {
+	
+	
+	private int cedulaCliente;
+	private String nombreCliente;
+	private double valor_venta;
+	
+	public int getCedulaCliente() {
+		return cedulaCliente;
+	}
+	public void setCedulaCliente(int cedulaCliente) {
+		cedulaCliente = cedulaCliente;
+	}
+	public String getNombreCliente() {
+		return nombreCliente;
+	}
+	public void setNombreCliente(String nombreCliente) {
+		nombreCliente = nombreCliente;
+	}
+	public double getValor_venta() {
+		return valor_venta;
+	}
+	public void setValor_venta(double valor_venta) {
+		valor_venta = valor_venta;
+	}
+
+
+}
